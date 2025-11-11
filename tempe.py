7fd42e939cb8d1959710ecb4ee3ce9ddacb28b1d
riyalagare8-rgb <riyalagare8@gmail.com>
2025-11-11 10:22:18 +0530

    Update tempe.py

--- tempe.py
+++ tempe.py
@@ -1,14 +1,16 @@
 import sys
 
 if len(sys.argv) != 2:
-    print("Usage: python temperature_check.py <temperature_in_Celsius>")
-    sys.exit(1)
-
-temperature = float(sys.argv[1])
-
-if temperature < 15:
+   system_name=sys.argv[0]
+   temp= sys.argv[1]
+else :
+   system_name=sys.argv[0]
+    temp="25"
+print(f"temperature: {temp}")
+if temp < "15":
     print("Cold")
-elif 15 <= temperature <= 30:
+elif temp>=15 and temp<=30:
     print("Normal")
 else:
     print("Hot")
+
