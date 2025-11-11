392874b9f5b73a9a7f2ac6931ca95de3a53ed8fa
riyalagare8-rgb <riyalagare8@gmail.com>
2025-11-11 09:56:06 +0530

    Refactor temperature input to use command line argument

--- tempe.py
+++ tempe.py
@@ -1,4 +1,11 @@
-temperature = float(input("Enter temperature in °C: "))
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: python temperature_check.py <temperature_in_Celsius>")
+    sys.exit(1)
+
+temperature = float(sys.argv[1])
 
 if temperature < 15:
     print("Cold")
